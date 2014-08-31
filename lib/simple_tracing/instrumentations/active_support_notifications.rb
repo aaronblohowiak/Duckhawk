edf4769d718244e1a1a8c8ebdeb3f8394011ffb1
Aaron Blohowiak <ablohowiak@fanhattan.com>
2014-08-30 21:55:05 -0700

    Another one where I forgot to call before :/

--- lib/simple_tracing/instrumentations/active_support_notifications.rb
+++ lib/simple_tracing/instrumentations/active_support_notifications.rb
@@ -3,6 +3,7 @@ ActiveSupport::Notifications.subscribe(/.*/) do |name, start, finish, id, payloa
     c = AbsoluteTime.now
 
     t = Trace.new(name, payload)
+    t.before
     t.finish = c
     t.start = c - (finish - start) #throw away the Time objects for our absolute time.
     t.after
