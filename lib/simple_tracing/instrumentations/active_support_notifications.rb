b174d475332e2a70c3c0b6fac912ef695bd3021a
Aaron Blohowiak <ablohowiak@fanhattan.com>
2014-08-30 20:02:01 -0700

    Fix activesupport notifications

--- lib/simple_tracing/instrumentations/active_support_notifications.rb
+++ lib/simple_tracing/instrumentations/active_support_notifications.rb
@@ -1,9 +1,10 @@
 ActiveSupport::Notifications.subscribe(/.*/) do |name, start, finish, id, payload|
-  return unless Trace.tracing?
-  c = AbsoluteTime.now
+  if Trace.tracing? #can't return cause we are in a block ;)
+    c = AbsoluteTime.now
 
-  t = Trace.new(name, payload)
-  t.finish = c
-  t.start = c - (finish - start) #throw away the Time objects for our absolute time.
-  t.after
+    t = Trace.new(name, payload)
+    t.finish = c
+    t.start = c - (finish - start) #throw away the Time objects for our absolute time.
+    t.after
+  end
 end
